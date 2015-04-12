74326ebd710063cf9278063d2679bf539146f83d
Joey Fowler <newfurniturey@gmail.com>
2015-04-12 17:03:38 -0400

    First extension of Model

---
+++ src/com/newfurniturey/mvc/app/models/Show.java
@@ -0,0 +1,11 @@
+package com.newfurniturey.mvc.app.models;
+
+import com.newfurniturey.mvc.app.Model;
+
+public class Show extends Model {
+	
+	public void bootstrap() {
+		// @todo write bootstrap
+	}
+	
+}
