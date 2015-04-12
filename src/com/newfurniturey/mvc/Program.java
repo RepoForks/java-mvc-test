4f089acb2f3940c61add6b58f3bb20e95a5cc2a2
Joey Fowler <newfurniturey@gmail.com>
2015-04-12 18:13:51 -0400

    Invoke the main application's bootstrap

--- src/com/newfurniturey/mvc/Program.java
+++ src/com/newfurniturey/mvc/Program.java
@@ -1,9 +1,17 @@
 package com.newfurniturey.mvc;
 
+import com.newfurniturey.mvc.app.App;
+
 public class Program {
-	
+
 	public static void main(String[] args) {
-		
+		try {
+			if (App.bootstrap()) {
+				// @todo do stuff!
+			}
+		} catch (ClassNotFoundException e) {
+			System.err.println("app error: " + e.getMessage());
+		}
 	}
 	
 }
