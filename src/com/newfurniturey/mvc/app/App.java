e2c0e388bc510d03e2428e90f6db644c7561ebe2
Joey Fowler <newfurniturey@gmail.com>
2015-04-12 18:13:18 -0400

    main application class

---
+++ src/com/newfurniturey/mvc/app/App.java
@@ -0,0 +1,37 @@
+package com.newfurniturey.mvc.app;
+
+import com.newfurniturey.mvc.app.models.Show;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class App {
+    private static String DB_NAME = "anime.sqlite";
+	
+	public static boolean bootstrap() throws ClassNotFoundException {
+        Class.forName("org.sqlite.JDBC");
+		
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection("jdbc:sqlite:" + DB_NAME);
+			(new Show(connection)).bootstrap();
+			
+			return true;
+		} catch (InvalidDatabaseException e) {
+			System.out.println("invalid database =[");
+			return false;
+		} catch (SQLException e) {
+			System.out.println("sql error: " + e.getMessage());
+			return false;
+		} finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                System.err.println("failed to close the connection: " + e.getMessage());
+            }
+		}
+	}
+	
+}
