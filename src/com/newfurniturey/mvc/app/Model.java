a1072d78e07ff4dda0ac75a2de318f135285d9ad
Joey Fowler <newfurniturey@gmail.com>
2015-04-12 17:03:15 -0400

    initial base model class

---
+++ src/com/newfurniturey/mvc/app/Model.java
@@ -0,0 +1,10 @@
+package com.newfurniturey.mvc.app;
+
+public abstract class Model {
+
+	/**
+	 * Initializes the model's database schema, if it doesn't already exist.
+	 */
+	public void bootstrap();
+	
+}
