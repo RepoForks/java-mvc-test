2b1aaa3c7694f8ee2b6c6e386daf8da7aa712c0d
Joey Fowler <newfurniturey@gmail.com>
2015-04-12 17:06:58 -0400

    Allow the model to be initialized with a db connection.

--- src/com/newfurniturey/mvc/app/Model.java
+++ src/com/newfurniturey/mvc/app/Model.java
@@ -1,6 +1,28 @@
 package com.newfurniturey.mvc.app;
 
+import java.sql.Connection;
+
 public abstract class Model {
+	/**
+	 * The database connection to use.
+	 */
+	protected Connection _connection = null;
+	
+	/**
+	 * Initializes the Model instance.
+	 */
+	public Model() {
+		// allow an empty construction, but nothing to do here =]
+	}
+	
+	/**
+	 * Initializes the Model instance.
+	 *
+	 * @param Connection conn The database connection to use.
+	 */
+	public Model(Connection conn) {
+		this._connection = conn;
+	}
 
 	/**
 	 * Initializes the model's database schema, if it doesn't already exist.
