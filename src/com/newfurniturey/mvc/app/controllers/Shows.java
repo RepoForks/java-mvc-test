2d97ebe93c8a924b926cc7ca7294c34fe5f5481e
Joey Fowler <newfurniturey@gmail.com>
2015-04-12 23:38:41 -0400

    Initial 'shows' controller

---
+++ src/com/newfurniturey/mvc/app/controllers/Shows.java
@@ -0,0 +1,13 @@
+package com.newfurniturey.mvc.app.controllers;
+
+import com.newfurniturey.mvc.app.Controller;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Shows extends Controller {
+	
+	public void actionPerformed(ActionEvent e) {
+		// @todo implement listener
+	}
+	
+}
