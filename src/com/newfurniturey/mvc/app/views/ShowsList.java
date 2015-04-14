d400aea53b37195bc83153040c44efbaed3024ab
Joey Fowler <newfurniturey@gmail.com>
2015-04-13 22:56:18 -0400

    subclassing stuffs

--- src/com/newfurniturey/mvc/app/views/ShowsList.java
+++ src/com/newfurniturey/mvc/app/views/ShowsList.java
@@ -2,8 +2,19 @@ package com.newfurniturey.mvc.app.views;
 
 import com.newfurniturey.mvc.app.View;
 import java.util.Observable;
+import javax.swing.JFrame;
 
 public class ShowsList extends View {
+	
+	public ShowsList() {
+		super();
+		_title = "Shows";
+	}
+	
+	@Override
+	public JFrame render() {
+		return super.render();
+	}
 	
 	@Override
 	public void update(Observable observable, Object object) {
