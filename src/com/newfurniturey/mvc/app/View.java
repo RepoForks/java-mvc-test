a61c6a258fb1d55099368b97bd39b84b48ca111e
Joey Fowler <newfurniturey@gmail.com>
2015-04-13 21:44:42 -0400

    adding empty implementation for subclasses who don't need it

--- src/com/newfurniturey/mvc/app/View.java
+++ src/com/newfurniturey/mvc/app/View.java
@@ -1,9 +1,12 @@
 package com.newfurniturey.mvc.app;
 
+import java.util.Observable;
 import java.util.Observer;
 
 public abstract class View implements Observer {
 	
-	
+    public void update(Observable observable, Object object) {
+		// empty override
+	}
 	
 }
