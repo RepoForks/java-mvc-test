544fc1f4741b66f57fe1841d2ea343a943b1ca5d
Joey Fowler <newfurniturey@gmail.com>
2015-04-13 22:33:45 -0400

    renamed file

--- src/com/newfurniturey/mvc/app/views/ShowsList.java
+++ src/com/newfurniturey/mvc/app/views/ShowsList.java
@@ -3,7 +3,7 @@ package com.newfurniturey.mvc.app.views;
 import com.newfurniturey.mvc.app.View;
 import java.util.Observable;
 
-public class Shows extends View {
+public class ShowsList extends View {
 	
 	@Override
 	public void update(Observable observable, Object object) {
