427a51adff481b02f99dcd415fc5cea45221ae6d
Joey Fowler <newfurniturey@gmail.com>
2015-04-13 23:25:59 -0400

    Adding search panel

--- src/com/newfurniturey/mvc/app/views/ShowsList.java
+++ src/com/newfurniturey/mvc/app/views/ShowsList.java
@@ -3,10 +3,12 @@ package com.newfurniturey.mvc.app.views;
 import com.newfurniturey.mvc.app.View;
 import java.awt.BorderLayout;
 import java.util.Observable;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 
 public class ShowsList extends View {
 	
@@ -20,10 +22,16 @@ public class ShowsList extends View {
 	@Override
 	public JFrame render() {
 		JFrame frame = super.render();
+		
+		// create the panel
 		JPanel panel = new JPanel();
 		panel.setLayout(new BorderLayout());
 		frame.getContentPane().add(panel);
 		
+		JPanel searchPanel = this._createSearchPanel();
+		panel.add(searchPanel, BorderLayout.NORTH);
+		
+		// add our data pane
 		JScrollPane dataPane = this._createDataTable();
 		panel.add(dataPane, BorderLayout.CENTER);
 		
@@ -44,5 +52,19 @@ public class ShowsList extends View {
 		
 		JTable table = new JTable(data, columns);
 		return new JScrollPane(table);
+	}
+	
+	protected JPanel _createSearchPanel() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		
+		// add the search box / button
+        JTextField searchField = new JTextField();
+        panel.add(searchField, BorderLayout.CENTER);
+
+        JButton searchButton = new JButton("Search");
+        panel.add(searchButton, BorderLayout.EAST);
+		
+		return panel;
 	}
 }
